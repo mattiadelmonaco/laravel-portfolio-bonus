ab41d76dc47671c21368e1a325c4872c694e8a7c
mattiadelmonaco <mattia.delmonaco@gmail.com>
2025-05-08 17:10:48 +0200

    feat: add links to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,20 @@
+import { NavLink } from "react-router";
+
 export default function Header() {
   return (
     <header className="bg-gray-800 shadow-lg">
-      <div className="w-[1200px] mx-auto py-5 flex justify-center items-center ">
-        <h1 className="text-white text-5xl">Header</h1>
+      <div className="w-[1200px] mx-auto py-5 flex justify-between items-center ">
+        <h1 className="text-white text-5xl">LOGO</h1>
+        <nav>
+          <ul className="flex gap-5 text-white text-2xl">
+            <li>
+              <NavLink to="/">HomePage</NavLink>
+            </li>
+            <li>
+              <NavLink to="/projects">I miei progetti</NavLink>
+            </li>
+          </ul>
+        </nav>
       </div>
     </header>
   );
